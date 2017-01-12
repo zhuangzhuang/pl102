ec60d3e4fd316ee39e35375c8e041351054c0256
zhuangzhuang <zhuangzhuang1988@outlook.com>
2017-01-12 22:44:36 +0800

    finish chapter3_basics

--- pl102/main.c
+++ pl102/main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <math.h>
 
+int add_together(int x, int y) {
+	int result = x + y;
+	return result;
+}
+
+typedef struct {
+	float x;
+	float y;
+} point;
 
 int main(int argc, char** argv) {
-	printf("Hello world\n");
+	int added = add_together(10, 18);
+
+	point p;
+	p.x = 0.1;
+	p.y = 10.0;
+
+	float length = sqrt(p.x * p.x + p.y * p.y);
+
+	int x = 0;
+	if (x > 10 && x < 100) {
+		puts("x is greater than 10 and less than 100!");
+	}
+	else {
+		puts("x is less than 11 or greater than 99!");
+	}
+
+	int i = 10;
+	while (i > 0)
+	{	
+		puts("Look Iteration");
+		i = i - 1;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		puts("Loop Iteration");
+	}
+
+
 	return 0;
 }
+
